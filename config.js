09746058998f6f0080ef5a02f9aaef90bdd25aa5
ra-lgt <raviajay9344@gmail.com>
2025-07-24 22:09:09 +0530

    updated the home page

---
+++ config.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+
+const CONFIG = {
+    PORT: process.env.PORT || 3000,
+    BACKEND_URL: process.env.BACKEND_URL
+}
+console.log(CONFIG);
+
+module.exports = { CONFIG };
--- server.js
+++ server.js
@@ -1,22 +1,31 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-const session = require('express-session');
-const {CONFIG} = require("./src/config/config");
-
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'src'));
+const session = require("express-session");
+const { CONFIG } = require("./src/config/config");
+const { getSettingDetails } = require("./src/services/settingService");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "src"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/assets', express.static(path.join(__dirname, 'src', 'assets')));
+app.use("/assets", express.static(path.join(__dirname, "src", "assets")));
 
+app.get("/", async (req, res) => {
+  const headers={
+    "content-type": "application/json",
+  }
+  const settingDetails = await getSettingDetails(headers);
 
-app.get("/",async(req,res)=>{
-    return res.render('index');
-})
-
+  if(!settingDetails?.success){
+    //handle error
+    return
+  }
+  const carouselData=settingDetails?.setting?.filter((item) => item.type === "home_carousel")
+  const eventData=settingDetails?.setting?.filter((item) => item.type === "event_info")
+  console.log(carouselData)
+  return res.render("index",{carouselData:carouselData || []});
+});
 
 app.listen(CONFIG.PORT, () => {
   console.log(`Server is running at http://localhost:${CONFIG.PORT}`);
-
 });
